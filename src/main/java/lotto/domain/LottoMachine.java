e3d0f8a760af2d69fcfbd60ed8d5c27df2de10b3
KimJS0328 <wjddkel328@gmail.com>
2024-04-03 11:42:28 +0900

    refactor: LottoMachine 로또 랜덤 로직에 Robert Floyd 알고리즘 적용

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -3,14 +3,20 @@ package lotto.domain;
 import static lotto.domain.Lotto.*;
 import static lotto.domain.LottoNumbers.*;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LottoMachine {
 
     public static final int LOTTO_PRICE = 1000;
+    private final Random random;
+
+    public LottoMachine() {
+        this.random = new Random();
+    }
 
     public List<Lotto> issue(int money) {
         validateMoney(money);
@@ -27,11 +33,20 @@ public class LottoMachine {
     }
 
     private Lotto issue() {
-        List<Integer> numbers = Stream.iterate(MIN_LOTTO_NUMBER, x -> x + 1)
-            .limit(MAX_LOTTO_NUMBER)
-            .collect(Collectors.toList());
-        Collections.shuffle(numbers);
-        List<Integer> lottoNumbers = numbers.subList(0, LOTTO_SIZE);
-        return new Lotto(lottoNumbers);
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int poolSize = MAX_LOTTO_NUMBER - LOTTO_SIZE + 1; poolSize <= MAX_LOTTO_NUMBER; ++poolSize) {
+            Integer number = random.nextInt(poolSize) + MIN_LOTTO_NUMBER;
+            numbers.add(nextNumber(numbers, number, poolSize));
+        }
+
+        return new Lotto(numbers);
+    }
+
+    private Integer nextNumber(List<Integer> numbers, Integer number, int poolSize) {
+        if (numbers.contains(number)) {
+            return poolSize;
+        }
+        return number;
     }
 }
