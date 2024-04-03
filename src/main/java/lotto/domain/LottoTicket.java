742884fe6a27905a724051365f8ca46f287605da
impala <dla4051@naver.com>
2024-04-03 18:12:21 +0900

    refactor(LottoTicket) LottoTicket과 Numbers의 상속관계 제거
    불필요한 상속관계를 제거하고 Numbers 클래스를 삭제

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -7,23 +7,38 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class LottoTicket extends Numbers {
+public class LottoTicket {
+
+    private final Set<LottoNumber> lottoNumbers;
 
     public LottoTicket(Set<LottoNumber> lottoNumbers) {
-        super(lottoNumbers);
-        if (lottoNumbers.size() != 6) {
-            throw new RuntimeException("로또 번호는 중복되지 않는 6개의 수로 구성되어야 합니다.");
-        }
+        validateLottoTicket(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
     public LottoTicket(List<LottoNumber> lottoNumbers) {
         this(new HashSet<>(lottoNumbers));
     }
 
-    public TicketDto toDto() {
-        return new TicketDto(lottoNumbers.stream()
-            .map(LottoNumber::toInteger)
-            .collect(Collectors.toList()));
+    public int compare (LottoTicket otherNumbers) {
+        return (int) lottoNumbers.stream()
+            .filter(otherNumbers::contains)
+            .count();
     }
 
+    public boolean contains(LottoNumber lottoNumber) {
+        return lottoNumbers.contains(lottoNumber);
+    }
+
+    public TicketDto toDto() {
+        return new TicketDto(lottoNumbers.stream()
+                .map(LottoNumber::toInteger)
+                .collect(Collectors.toList()));
+    }
+
+    private static void validateLottoTicket(Set<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new RuntimeException("로또 번호는 중복되지 않는 6개의 수로 구성되어야 합니다.");
+        }
+    }
 }
--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -2,14 +2,50 @@ package lotto.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 
 public class LottoTicketTest {
+
+    @Test
+    public void compare는_중복되는_Number의_개수를_반환한다() {
+        // given
+        LottoTicket numbers = new LottoTicket(Stream.of(1, 2, 3, 4, 5, 6)
+                .map(LottoNumber::new)
+                .collect(Collectors.toSet()));
+        LottoTicket otherNumbers = new LottoTicket(Stream.of(1, 2, 3, 7, 8, 9)
+                .map(LottoNumber::new)
+                .collect(Collectors.toSet()));
+
+        // when
+        int count = numbers.compare(otherNumbers);
+
+        // then
+        Assertions.assertThat(count).isEqualTo(3);
+    }
+
+    @Test
+    public void contains는_해당Number의_포함여부를_반환한다() {
+        // given
+        LottoTicket numbers = new LottoTicket(Stream.of(1, 2, 3, 4, 5, 6)
+                .map(LottoNumber::new)
+                .collect(Collectors.toSet()));
+        LottoNumber lottoNumber = new LottoNumber(1);
+
+        // when
+        boolean isContaining = numbers.contains(lottoNumber);
+
+        // then
+        Assertions.assertThat(isContaining).isTrue();
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"1,2,3,4,5,6"})
     public void 로또_번호는_중복되지_않는_6개의_수로_이루어져_있다(String numbers) {
