41d02c82b6c004018982f0604305287b1786a46b
impala <dla4051@naver.com>
2024-04-03 17:41:03 +0900

    refactor(WinningNumbers) WinningNumbers와 Numbers의 상속관계 제거
    WinningNumbers가 Numbers를 상속받는 구조에서 WinningNumbers가 LottoTicket을 가지는 구조로 변경

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,24 +1,24 @@
 package lotto.domain;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-public class WinningNumbers extends Numbers{
-    private final LottoNumber bonusLottoNumber;
+public class WinningNumbers {
 
-    public WinningNumbers(List<LottoNumber> regularLottoNumbers, LottoNumber bonusLottoNumber) {
-        this(new HashSet<>(regularLottoNumbers), bonusLottoNumber);
+    private final LottoTicket winningTicket;
+    private final LottoNumber bonusNumber;
+
+    public WinningNumbers(List<LottoNumber> winningNumbers, LottoNumber bonusNumber) {
+        this(new LottoTicket(winningNumbers), bonusNumber);
     }
 
-    public WinningNumbers(Set<LottoNumber> regularLottoNumbers, LottoNumber bonusLottoNumber) {
-        super(regularLottoNumbers);
-        this.bonusLottoNumber = bonusLottoNumber;
-        validateWinningNumber(regularLottoNumbers, bonusLottoNumber);
+    public WinningNumbers(LottoTicket winningTicket, LottoNumber bonusNumber) {
+        validateWinningNumber(winningTicket, bonusNumber);
+        this.winningTicket = winningTicket;
+        this.bonusNumber = bonusNumber;
     }
 
     public Prize checkWinning(LottoTicket lottoTicket) {
-        int matchCount = lottoTicket.compare(this);
+        int matchCount = winningTicket.compare(lottoTicket);
         if (matchCount <= 2) {
             return Prize.NOTHING;
         }
@@ -32,21 +32,17 @@ public class WinningNumbers extends Numbers{
         if (matchCount == 4) {
             return Prize.FOURTH;
         }
-        if (matchCount == 5 && !lottoTicket.contains(bonusLottoNumber)) {
+        if (matchCount == 5 && !lottoTicket.contains(bonusNumber)) {
             return Prize.THIRD;
         }
-        if (matchCount == 5 && lottoTicket.contains(bonusLottoNumber)) {
+        if (matchCount == 5 && lottoTicket.contains(bonusNumber)) {
             return Prize.SECOND;
         }
         return Prize.FIRST;
     }
 
-    private void validateWinningNumber(Set<LottoNumber> regularLottoNumbers, LottoNumber bonusLottoNumber) {
-        if (regularLottoNumbers.size() != 6) {
-            throw new RuntimeException("당첨번호는 중복되지 않는 6개의 수로 구성되어야 합니다.");
-        }
-
-        if (regularLottoNumbers.contains(bonusLottoNumber)) {
+    private void validateWinningNumber(LottoTicket winningTicket, LottoNumber bonusLottoNumber) {
+        if (winningTicket.contains(bonusLottoNumber)) {
             throw new RuntimeException("보너스볼은 당첨 번호와 중복되지 않아야 합니다.");
         }
     }
