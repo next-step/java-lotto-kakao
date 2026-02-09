6aac4f2dee667bbd2f193a2c9a8ea95e41e03a37
wayne.lee <wayne.lee@kakaocorp.com>
2026-02-09 15:27:36 +0900

    feat(Lotto): add test case for Lotto 1th~5th

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,28 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Lotto {
-    public Lotto(int... numbers) {
+    private final Set<Integer> lottoNumberSet;
+
+    public Lotto(Integer... lottoNumbers) {
+        this(Set.of(lottoNumbers));
     }
 
-    public LottoRank lottery(Lotto buy) {
-        return LottoRank.LOSE;
+    public Lotto(Set<Integer> lottoNumberSet) {
+        this.lottoNumberSet = lottoNumberSet;
     }
+
+    public int matchCount(Lotto buy) {
+        Set<Integer> intersection = new HashSet<>(lottoNumberSet);
+        intersection.retainAll(buy.lottoNumberSet);
+
+        return intersection.size();
+    }
+
+    public boolean hasBonus(int bonus) {
+        return lottoNumberSet.contains(bonus);
+    }
+
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,15 +1,57 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LottoTest {
+    private WinLotto win;
+
+    @BeforeEach
+    void setUp() {
+        win = new WinLotto(7,1,2,3,4,5,6);
+    }
+
     @Test
     void lose() {
-        Lotto win = new Lotto(1,2,3,4,5,6);
         Lotto buy = new Lotto(7,8,9,10,11,12);
         LottoRank rank = win.lottery(buy);
         assertThat(rank).isEqualTo(LottoRank.LOSE);
+    }
+
+    @Test
+    void fifthWin() {
+        Lotto buy = new Lotto(1,2,3,10,11,12);
+        LottoRank rank = win.lottery(buy);
+        assertThat(rank).isEqualTo(LottoRank.FIFTH);
+    }
+
+    @Test
+    void fourthWin() {
+        Lotto buy = new Lotto(1,2,3,4,11,12);
+        LottoRank rank = win.lottery(buy);
+        assertThat(rank).isEqualTo(LottoRank.FOURTH);
+    }
+
+    @Test
+    void thirdWin() {
+        Lotto buy = new Lotto(1,2,3,4,5,12);
+        LottoRank rank = win.lottery(buy);
+        assertThat(rank).isEqualTo(LottoRank.THIRD);
+    }
+
+    @Test
+    void secondWin() {
+        Lotto buy = new Lotto(1,2,3,4,5,7);
+        LottoRank rank = win.lottery(buy);
+        assertThat(rank).isEqualTo(LottoRank.SECOND);
+    }
+
+    @Test
+    void firstWin() {
+        Lotto buy = new Lotto(1,2,3,4,5,6);
+        LottoRank rank = win.lottery(buy);
+        assertThat(rank).isEqualTo(LottoRank.FIRST);
     }
 }
