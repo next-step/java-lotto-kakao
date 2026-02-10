0e60d89d4bb16855668b375dabd0ed0667d0059f
wayne.lee <wayne.lee@kakaocorp.com>
2026-02-10 18:30:59 +0900

    feat(WinLotto): WinLotto 로직 구현 및 테스트 코드 작성

--- src/main/java/lotto/WinLotto.java
+++ src/main/java/lotto/WinLotto.java
@@ -1,41 +1,27 @@
 package lotto;
 
-import java.util.Set;
-
 public class WinLotto {
-    private final int bonus;
+    private final LottoNumber bonus;
     private final Lotto win;
 
     public WinLotto(int bonus, Integer... lottoNumbers) {
-        this(bonus, (new Lotto(Set.of(lottoNumbers))));
+        this(new LottoNumber(bonus), (new Lotto(lottoNumbers)));
     }
 
-    public WinLotto(int bonus, Lotto win) {
+    public WinLotto(LottoNumber bonus, Lotto win) {
+        validate(bonus, win);
         this.bonus = bonus;
         this.win = win;
     }
 
+    private void validate(LottoNumber bonus, Lotto win) {
+        if (win.contains(bonus)) {
+            throw new IllegalArgumentException("보너스 볼은 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
     public LottoRank lottery(Lotto buy) {
         int count = buy.matchCount(win);
-
         return LottoRank.searchRank(count, buy.hasBonus(bonus));
-
-//        if (count == 6) {
-//            return LottoRank.FIRST;
-//        }
-//        if (count == 5 && buy.hasBonus(bonus)) {
-//            return LottoRank.SECOND;
-//        }
-//        if (count == 5) {
-//            return LottoRank.THIRD;
-//        }
-//        if (count == 4) {
-//            return LottoRank.FOURTH;
-//        }
-//        if (count == 3) {
-//            return LottoRank.FIFTH;
-//        }
-//
-//        return LottoRank.LOSE;
     }
 }
