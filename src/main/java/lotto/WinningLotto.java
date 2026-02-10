5fc3d1fe1704f4a2516fdc25b0fc7631b7b2f624
mingmingmon <mingmingmon@naver.com>
2026-02-10 12:25:41 +0900

    feat(WinningLotto): 번호 비교 메소드/테스트 구현

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -15,4 +15,14 @@ public class WinningLotto {
 	List<Integer> getNumbers() {
 		return this.numbers;
 	}
+
+	int checkNumbers(List<Integer> lottoNumbers) {
+		return (int) lottoNumbers.stream()
+			.filter(numbers::contains)
+			.count();
+	}
+
+	boolean isContainBonus(List<Integer> lottoNumbers) {
+		return lottoNumbers.contains(bonus);
+	}
 }
--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -9,12 +9,27 @@ import org.junit.jupiter.api.Test;
 
 public class WinningLottoTest {
 
+	List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+	int bonus = 7;
+	WinningLotto winningLotto = new WinningLotto(numbers, bonus);
+
 	@Test
 	void winningLottoTest() {
-		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-		int bonus = 7;
-		WinningLotto winningLotto = new WinningLotto(numbers, bonus);
 		List<Integer> winningLottoNumber = winningLotto.getNumbers();
 		assertThat(winningLottoNumber.size()).isEqualTo(6);
+	}
+
+	@Test
+	void lotteryNumberListCheckerTest() {
+		List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+		int hits = winningLotto.checkNumbers(lottoNumbers);
+		assertThat(hits).isEqualTo(6);
+	}
+
+	@Test
+	void lotteryBonusCheckerTest() {
+		List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 7);
+		boolean isContainBonus = winningLotto.isContainBonus(lottoNumbers);
+		assertThat(isContainBonus).isEqualTo(true);
 	}
 }
