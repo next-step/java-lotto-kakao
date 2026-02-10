5c43c1aaaabfe14c377b120929e049d01cb5cd50
wayne.lee <wayne.lee@kakaocorp.com>
2026-02-10 18:30:59 +0900

    feat(WinLotto): WinLotto 로직 구현 및 테스트 코드 작성

--- src/main/java/lotto/WinLotto.java
+++ src/main/java/lotto/WinLotto.java
@@ -9,8 +9,15 @@ public class WinLotto {
     }
 
     public WinLotto(LottoNumber bonus, Lotto win) {
+        validate(bonus, win);
         this.bonus = bonus;
         this.win = win;
+    }
+
+    private void validate(LottoNumber bonus, Lotto win) {
+        if (win.contains(bonus)) {
+            throw new IllegalArgumentException("보너스 볼은 당첨 번호와 중복될 수 없습니다.");
+        }
     }
 
     public LottoRank lottery(Lotto buy) {
