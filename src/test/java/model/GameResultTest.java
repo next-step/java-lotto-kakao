4603c02faf05c2e215b312fe897c602cac4326b2
youngho98 <youngho.dev@gmail.com>
2026-02-11 15:36:50 +0900

    refactor: test 코드 포맷 & 변수명 리팩토링

---
+++ src/test/java/model/GameResultTest.java
@@ -0,0 +1,52 @@
+package model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class WinStatsTest {
+    GameScore gameScore = new GameScore(7, Arrays.asList(1, 2, 3, 4, 5, 6));
+
+    @Test
+    void ticketLevel() {
+        List<Ticket> tickets = Arrays.asList(
+                new Ticket(1, 2, 3, 4, 5, 6),
+                new Ticket(1, 2, 3, 4, 5, 7),
+                new Ticket(1, 2, 3, 4, 5, 8),
+                new Ticket(1, 2, 3, 4, 9, 8),
+                new Ticket(1, 2, 3, 10, 9, 8),
+                new Ticket(7, 8, 9, 10, 11, 12)
+        );
+        WinLevel[] winLevels = {
+                WinLevel.FIRST,
+                WinLevel.SECOND,
+                WinLevel.THIRD,
+                WinLevel.FOURTH,
+                WinLevel.FIFTH,
+                WinLevel.LOSER,
+        };
+        StatsBoard statsBoard = new StatsBoard(gameScore, tickets);
+
+
+        for (int i = 0; i < 6; i++) {
+            assertThat(tickets.get(i).getWinLevel(gameScore)).isEqualTo(winLevels[i]);
+        }
+    }
+
+    @Test
+    void profitRatioMaker() {
+        StatsBoard statsBoard = new StatsBoard(gameScore, Arrays.asList(
+                new Ticket(1, 2, 3, 4, 5, 6),
+                new Ticket(1, 2, 3, 4, 5, 7),
+                new Ticket(1, 2, 3, 4, 5, 8),
+                new Ticket(1, 2, 3, 4, 9, 8),
+                new Ticket(1, 2, 3, 10, 9, 8),
+                new Ticket(7, 8, 9, 10, 11, 12)
+        ));
+        Double profitRatio = statsBoard.getProfitRatio();
+        assertThat(profitRatio).isEqualTo(338592.5);
+    }
+}
