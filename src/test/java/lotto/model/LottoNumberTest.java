18271ea7357bc6fe99c9fa527507937b69513ce8
semi-colon-00 <semi.colon@kakaocorp.com>
2026-02-11 17:49:08 +0900

    test: LottoNumberTest 보강

--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -11,15 +11,33 @@ class LottoNumberTest {
     @Test
     @DisplayName("올바른 번호(1-45)일 때")
     public void validateNumber(){
-        assertThatCode(() -> new LottoNumber(5));
+        assertThatCode(() -> new LottoNumber(5)).doesNotThrowAnyException();
     }
 
     @Test
-    @DisplayName("올바르지 않은 번호일 때")
-    public void invalidateNumber(){
-        assertThatThrownBy(() -> new LottoNumber(64))
-                .isInstanceOf(RuntimeException.class)
-                .hasMessage("로또 번호는 1-45 사이 값이어야 합니다");
+    @DisplayName("경계값 - 최소값 1")
+    public void boundaryMin() {
+        assertThatCode(() -> new LottoNumber(1)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("경계값 - 최대값 45")
+    public void boundaryMax() {
+        assertThatCode(() -> new LottoNumber(45)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("경계값 - 0은 유효하지 않음")
+    public void boundaryBelowMin() {
+        assertThatThrownBy(() -> new LottoNumber(0))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("경계값 - 46은 유효하지 않음")
+    public void boundaryAboveMax() {
+        assertThatThrownBy(() -> new LottoNumber(46))
+                .isInstanceOf(RuntimeException.class);
     }
 
     @Test
@@ -28,6 +46,14 @@ class LottoNumberTest {
         LottoNumber number1 = new LottoNumber(1);
         LottoNumber number2 = new LottoNumber(1);
         assertThat(number1).isEqualTo(number2);
+    }
+
+    @Test
+    @DisplayName("로또 번호 일치 여부 검사 - 일치하지 않을때")
+    public void isNotEqual(){
+        LottoNumber number1 = new LottoNumber(1);
+        LottoNumber number2 = new LottoNumber(2);
+        assertThat(number1).isNotEqualTo(number2);
     }
 
 }
