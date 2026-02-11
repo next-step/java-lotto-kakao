fbd271efaf2f310ea059c858e7172a876f15c9fb
김덕윤 <duk9741@gmail.com>
2026-02-11 14:50:27 +0900

    refactor: WinningNumbers 인덴트 제거

--- src/main/java/com/kakao/onboarding/precourse/albusduke/lotto/domain/WinningNumbers.java
+++ src/main/java/com/kakao/onboarding/precourse/albusduke/lotto/domain/WinningNumbers.java
@@ -2,27 +2,35 @@ package com.kakao.onboarding.precourse.albusduke.lotto.domain;
 
 public class WinningNumbers {
 
-    private static final String BONUS_NUMBER_DUPLICATED_ERR_MSG = "보너스 번호는 당첨 번호에 포함될 수 없습니다.";
+	private static final String BONUS_NUMBER_DUPLICATED_ERR_MSG = "보너스 번호는 당첨 번호에 포함될 수 없습니다.";
 
-    private final LottoNumbers winningNumbers;
-    private final LottoNumber bonusNumber;
+	private final LottoNumbers winningNumbers;
+	private final LottoNumber bonusNumber;
 
-    public WinningNumbers(LottoNumbers winningNumbers, LottoNumber bonusNumber) {
-        validateBonusInNumbers(winningNumbers, bonusNumber);
-        this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
-    }
+	public WinningNumbers(LottoNumbers winningNumbers, LottoNumber bonusNumber) {
+		validateBonusInNumbers(winningNumbers, bonusNumber);
+		this.winningNumbers = winningNumbers;
+		this.bonusNumber = bonusNumber;
+	}
 
-    private static void validateBonusInNumbers(LottoNumbers lottoNumbers, LottoNumber bonusNumber) {
-        if(lottoNumbers.hasNumber(bonusNumber)) {
-            throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATED_ERR_MSG);
-        }
-    }
+	private static void validateBonusInNumbers(LottoNumbers lottoNumbers, LottoNumber bonusNumber) {
+		if (lottoNumbers.hasNumber(bonusNumber)) {
+			throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATED_ERR_MSG);
+		}
+	}
 
-    public GameResult calculateResult(LottoNumbers game) {
-        int matchingCount = game.countMatchingNumbers(winningNumbers);
-        int bonusMatchingCount = game.hasNumber(bonusNumber) ? 1 : 0;
+	public GameResult calculateResult(LottoNumbers game) {
+		int matchingCount = game.countMatchingNumbers(winningNumbers);
+		int bonusMatchingCount = calculateBonusMatchCount(game);
 
-        return new GameResult(matchingCount, bonusMatchingCount);
-    }
+		return new GameResult(matchingCount, bonusMatchingCount);
+	}
+
+	private int calculateBonusMatchCount(LottoNumbers game) {
+		if (game.hasNumber(bonusNumber)) {
+			return 1;
+		}
+
+		return 0;
+	}
 }
