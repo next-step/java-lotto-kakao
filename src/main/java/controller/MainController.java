27f496738b1b0575d1a122ff0dbcb25db5a563c6
greemi17 <greem.17@kakaocorp.com>
2026-02-11 12:48:34 +0900

    refactor: 입출력 에러 검증

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -14,6 +14,7 @@ import view.TicketBoothView;
 public class MainController {
 	public static final int MAX_BALL_NUMBER = 45;
 	public static final int MIN_BALL_NUMBER = 1;
+	public static final int BALL_COUNT = 6;
 	TicketBooth ticketBooth = new TicketBooth();
 	private final TicketBoothView ticketBoothView = new TicketBoothView();
 	private final GameScoreView gameScoreView = new GameScoreView();
@@ -24,40 +25,29 @@ public class MainController {
 	}
 
 	private void ticketBoothRender() {
-		ticketBoothView.showInputPriceMessage();
-		Integer price = ticketBoothView.inputTicketPrice();
-		List<Ticket> tickets = ticketBooth.issueTickets(price);
-		ticketBoothView.showTicketInfo(tickets);
-		gameScoreRender(tickets);
+		try {
+			ticketBoothView.showInputPriceMessage();
+			Integer price = ticketBoothView.inputTicketPrice();
+			List<Ticket> tickets = ticketBooth.issueTickets(price);
+			ticketBoothView.showTicketInfo(tickets);
+			gameScoreRender(tickets);
+		} catch (IllegalArgumentException e) {
+			ticketBoothView.showErrorMessage(e);
+			ticketBoothRender();
+		}
 	}
 
 	private void gameScoreRender(List<Ticket> tickets) {
-		gameScoreView.showInputWinNumberMessage();
-
-		List<Integer> winNumbers = gameScoreView.inputWinNumber();
-		if(winNumbers.size() != 6) {
-			gameScoreView.showErrorMessage(new IllegalArgumentException("숫자 6개를 입력해주세요"));
+		try {
+			gameScoreView.showInputWinNumberMessage();
+			List<Integer> winNumbers = gameScoreView.inputWinNumber(BALL_COUNT, MIN_BALL_NUMBER, MAX_BALL_NUMBER);
+			gameScoreView.showInputBonusBall();
+			Integer bonusBall = gameScoreView.inputBonusBall(MIN_BALL_NUMBER, MAX_BALL_NUMBER);
+			statBoardRender(new GameScore(bonusBall, winNumbers), tickets);
+		} catch (IllegalArgumentException e) {
+			gameScoreView.showErrorMessage(e);
 			gameScoreRender(tickets);
-			return;
 		}
-
-		for (Integer winNumber : winNumbers) {
-			if(winNumber < MIN_BALL_NUMBER || winNumber > MAX_BALL_NUMBER) {
-				gameScoreView.showErrorMessage(new IllegalArgumentException("1부터 45까지를 입력하세요"));
-				gameScoreRender(tickets);
-				return;
-			}
-		}
-
-		gameScoreView.showInputBonusBall();
-		Integer bonusBall = gameScoreView.inputBonusBall();
-		if(bonusBall < MIN_BALL_NUMBER || bonusBall > MAX_BALL_NUMBER) {
-			gameScoreView.showErrorMessage(new IllegalArgumentException("1부터 45까지를 입력하세요"));
-			gameScoreRender(tickets);
-			return;
-		}
-
-		statBoardRender(new GameScore(bonusBall, winNumbers), tickets);
 	}
 
 	private void statBoardRender(GameScore score, List<Ticket> tickets) {
--- src/main/java/view/GameScoreView.java
+++ src/main/java/view/GameScoreView.java
@@ -11,34 +11,38 @@ public class GameScoreView {
 		System.out.println("지난 주 당첨 번호를 입력해 주세요.");
 	}
 
-	public List<Integer> inputWinNumber() {
+	public List<Integer> inputWinNumber(Integer ballCount, Integer minBallNumber, Integer maxBallNumber) {
 		String rawNumbers = scanner.nextLine();
-		try {
+			if (!rawNumbers.matches("^[0-9][0-9, ]*$")) {
+				throw new IllegalArgumentException("숫자와 ','만 입력 가능합니다.");
+			}
 			List<Integer> result = new ArrayList<>();
 			for (String rawNumber : rawNumbers.split(",")) {
-				result.add(Integer.parseInt(rawNumber.trim()));
+				int number = Integer.parseInt(rawNumber.trim());
+				if(number < minBallNumber || number > maxBallNumber) throw new IllegalArgumentException("1부터 45까지를 입력하세요");
+				result.add(number);
 			}
-
-			if(result.size() != 6) {
+			if(result.size() != ballCount) {
 				throw new IllegalArgumentException("숫자 6개를 입력해주세요.");
 			}
 			return result;
-		} catch (Exception e) {
-			throw new IllegalArgumentException("구분자는 ','로 작성해주세요.");
-		}
+
 	}
 
 	public void showInputBonusBall() {
 		System.out.println("보너스 볼을 입력해 주세요.");
 	}
 
-	public Integer inputBonusBall() {
-		try {
+	public Integer inputBonusBall(Integer minBallNumber, Integer maxBallNumber) {
+
 			String rawNumber = scanner.nextLine();
-			return Integer.parseInt(rawNumber);
-		} catch (Exception e) {
-			throw new IllegalArgumentException("숫자를 입력하세요!");
-		}
+			if (!rawNumber.matches("[0-9]*$")) {
+				throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+			}
+			Integer result =  Integer.parseInt(rawNumber);
+			if(result < minBallNumber || result > maxBallNumber) throw new IllegalArgumentException("1부터 45까지를 입력하세요");
+			return result;
+
 	}
 
 	public void showErrorMessage(IllegalArgumentException e) {
