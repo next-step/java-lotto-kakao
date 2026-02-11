5d1e3f76878b50302ea0fb526bd68ff8831d68b9
sapiensxxv <jhspacelover@naver.com>
2026-02-11 22:52:47 +0900

    test: 덧셈 계산기 테스트

--- src/test/java/calculator/InputParserTest.java
+++ src/test/java/calculator/InputParserTest.java
@@ -6,52 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class InputParserTest {
 
-    @Test
-    @DisplayName("커스텀 구분자가 없는 정상 입력인 경우")
-    void inputTest1() {
-        String successInput = "1,2,3";
-        InputParser inputParser = new InputParser(successInput);
 
-        Assertions.assertThat(inputParser.getNumber().getNumbers()).containsExactly(1, 2, 3);
-    }
-
-    @Test
-    @DisplayName("커스텀 구분자가 포함된 정상 입력인 경우")
-    void inputTest2() {
-        Separator input = new Separator("//;\n,1,2,3");
-
-    }
-
-    @Test
-    @DisplayName("등록한 구분자 이외의 구분자를 사용한 경우")
-    void inputExceptionTest3() {
-        Assertions.assertThatThrownBy(() -> {
-            InputParser inputParser = new InputParser("1^2,3");
-        }).isInstanceOf(IllegalArgumentException.class).hasMessage("등록되지 않은 커스텀 구분자가 입력되었습니다.");
-    }
-
-    @Test
-    @DisplayName("커스텀 구분자가 숫자인 경우")
-    void inputExceptionTest4() {
-        Assertions.assertThatThrownBy(() -> {
-            Separator input = new Separator("//6\n1,2,3");
-        }).isInstanceOf(IllegalArgumentException.class).hasMessage("커스텀 구분자는 숫자가 될 수 없습니다.");
-    }
-
-    @Test
-    @DisplayName("커스텀 구분자가 숫자인 경우")
-    void inputExceptionTest5() {
-        Assertions.assertThatThrownBy(() -> {
-            Separator input = new Separator("///;\n1;2;3");
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("구분자를 연속적으로 사용한 경우")
-    void inputExceptionTest6() {
-        Assertions.assertThatThrownBy(() -> {
-            InputParser inputParser = new InputParser("1,2,,3");
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
 
 }
--- src/test/java/calculator/NumberTest.java
+++ src/test/java/calculator/NumberTest.java
@@ -6,30 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class NumberTest {
 
-    @Test
-    @DisplayName("입력에 문자열이 포함된 예외 경우")
-    void inputExceptionTest1() {
-        Assertions.assertThatThrownBy(() -> {
-            calculator.Number number = new calculator.Number("1 2 a");
-        }).isInstanceOf(RuntimeException.class).hasMessage("숫자 형식이 아닙니다.");
-    }
 
-    @Test
-    @DisplayName("음수를 입력받은 경우")
-    void inputExceptionTest2() {
-        Assertions.assertThatThrownBy(() -> {
-                    calculator.Number number = new calculator.Number("-1 2 3");
-                })
-                .isInstanceOf(RuntimeException.class)
-                .hasMessage("음수가 입력되었습니다.");
-    }
-
-    @Test
-    @DisplayName("입력 받은 값을 계산 할 수 있다.")
-    void calculationTest() {
-        calculator.Number number = new Number("1 2 3");
-
-        Assertions.assertThat(number.calculate()).isEqualTo(6);
-    }
 
 }
