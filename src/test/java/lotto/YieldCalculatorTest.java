1e32314e0265e50c107996bee572ffe71075f6d5
seonwoonam <potatomoney716@gmail.com>
2026-02-12 14:26:35 +0900

    fix : 수익률 계산 테스트 파라미터 순서 변경 테스트에서도 수정

--- src/test/java/lotto/YieldCalculatorTest.java
+++ src/test/java/lotto/YieldCalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class YieldCalculatorTest {
     @Test
     void 수익률_계산_테스트() {
-        Assertions.assertThat(YieldCalculator.calculate(14000, 5000))
+        Assertions.assertThat(YieldCalculator.calculate(5000, 14000))
                 .isEqualTo(0.35);
     }
 }
