ebd5a6cfabc1157ff1b96bf90197f4c850be0b4a
seungwone <aahhll654@gmail.com>
2026-02-16 00:25:53 +0900

    refactor(LottoShopTest): LottoShop 테스트 관심사별 분리 및 메소드명 구체화

--- src/test/java/lotto/LottoShopTest.java
+++ src/test/java/lotto/LottoShopTest.java
@@ -8,24 +8,35 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LottoShopTest {
-    @Test
-    void purchaseBundle() {
-        Money total = Money.won(14500L);
-        long count = total.calculatePurchasableCount(LottoShop.PRICE);
-        Money paid = LottoShop.PRICE.times(count);
-        Money change = total.minus(paid);
+    private static final Money MONEY_14500 = Money.won(14500L);
 
-        LottoBundlePurchase lottoBundlePurchase = LottoShop.purchaseBundle(total);
-        assertThat(count).isEqualTo(lottoBundlePurchase.lottoBundle().size());
-        assertThat(paid).isEqualTo(lottoBundlePurchase.paid());
-        assertThat(change).isEqualTo(lottoBundlePurchase.change());
+    @Test
+    void createsBundleWithPurchasableCount() {
+        LottoBundlePurchase purchase = LottoShop.purchaseBundle(MONEY_14500);
+        assertThat(purchase.lottoBundle().size()).isEqualTo(14);
     }
 
     @Test
-    void purchaseBundleFail() {
+    void calculatePaid() {
+        LottoBundlePurchase purchase = LottoShop.purchaseBundle(MONEY_14500);
+        assertThat(purchase.paid()).isEqualTo(Money.won(14000L));
+    }
+
+    @Test
+    void calculateChange() {
+        LottoBundlePurchase purchase = LottoShop.purchaseBundle(MONEY_14500);
+        assertThat(purchase.change()).isEqualTo(Money.won(500L));
+    }
+
+    @Test
+    void purchaseBundleFailWhenAmountIsZero() {
         assertThrows(IllegalArgumentException.class,
-                () -> LottoShop.purchaseBundle(Money.won(0)));
+                () -> LottoShop.purchaseBundle(Money.won(0L)));
+    }
+
+    @Test
+    void purchaseBundleFailWhenAmountIsLessThanPrice() {
         assertThrows(IllegalArgumentException.class,
-                () -> LottoShop.purchaseBundle(Money.won(999)));
+                () -> LottoShop.purchaseBundle(Money.won(999L)));
     }
 }
