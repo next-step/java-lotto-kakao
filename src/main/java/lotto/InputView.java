5d5860368111caf0e6809db499862f75f11799fb
seonwoonam <potatomoney716@gmail.com>
2026-02-16 02:23:30 +0900

    feat : 수동으로 구매할 번호 입력받기 구현

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -19,6 +21,15 @@ public class InputView {
     public static int readManualCount() {
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
         return parseNumber(scanner.nextLine());
+    }
+
+    public static List<String> readManualNumbers(int count){
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<String> lines = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lines.add(scanner.nextLine());
+        }
+        return lines;
     }
 
     public static int readingBonusNumber(){
