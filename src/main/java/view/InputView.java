742206033f6986663ae29f1d17eba23ec96cd7fb
jeongseongwon99 <sw990320@naver.com>
2026-02-18 22:36:33 +0900

    feat(InputView): 수동 구매 count 기능 추가 코드 및 테스트 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -12,6 +14,26 @@ public class InputView {
     public String readPurchaseMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         return scanner.nextLine();
+    }
+
+    public String readManualCount() {
+        System.out.println();
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public List<String> readManualLottoNumbers(int manualCount) {
+        List<String> rawManualLottos = new ArrayList<>();
+        if (manualCount == 0) {
+            return rawManualLottos;
+        }
+
+        System.out.println();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        for (int i = 0; i < manualCount; i++) {
+            rawManualLottos.add(scanner.nextLine());
+        }
+        return rawManualLottos;
     }
 
     public String readWinningNumbers() {
