6aa39ab35706f1aa3f5402f6173027df53f7278a
sapiensxxv <jhspacelover@naver.com>
2026-02-18 14:05:07 +0900

    test: Lotto domain test

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottosTest {
+
+    @Test
+    @DisplayName("merge는 두 묶음을 합친 새로운 Lottos를 반환한다.")
+    void merge() {
+        Lottos a = Lottos.from(List.of(new Lotto(1, 2, 3, 4, 5, 6)));
+        Lottos b = Lottos.from(List.of(
+                new Lotto(7, 8, 9, 10, 11, 12),
+                new Lotto(13, 14, 15, 16, 17, 18)
+        ));
+
+        Lottos merged = a.merge(b);
+
+        assertThat(merged.size()).isEqualTo(3);
+    }
+
+}
