e92f1f6ee8dfc8b8811f5f8102b5dd0f4afb3948
youngho <161284817+youngho98@users.noreply.github.com>
2026-02-19 11:41:36 +0900

    feat: 로또 수동 입력 구현

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -12,24 +12,30 @@ import view.OutputView;
 import java.util.ArrayList;
 import java.util.List;
 
+import static model.Lottos.COST;
+
 public class MainController {
 
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
     public void run() {
-        Lottos lottos = issueLottos();
+        Lottos lottos = getLottos();
         LottoResult lottoResult = getLottoResult();
         LottoStatistics lottoStatistics = new LottoStatistics(lottos.getLottos(), lottoResult);
         printLottoStatistics(lottoStatistics);
     }
 
-    private Lottos issueLottos() {
+    private Lottos getLottos() {
         while (true) {
             try {
                 outputView.printInputPriceMessage();
-                int price = inputView.getNumber();
-                Lottos lottos = new Lottos(price);
+                int price = inputPrice();
+                outputView.printInputManualCountMessage();
+                int manualCount = inputManualCount(price);
+                outputView.printInputManualNumbersMessage();
+                List<LottoNumbers> lottoNumbersList = inputLottoNumbersList(manualCount);
+                Lottos lottos = new Lottos(price, manualCount, lottoNumbersList);
                 outputView.printLottos(lottos);
                 return lottos;
             } catch (IllegalArgumentException e) {
@@ -41,8 +47,10 @@ public class MainController {
     private LottoResult getLottoResult() {
         while (true) {
             try {
-                LottoNumbers mainNumbers = inputMainNumbers();
-                LottoNumber bonusNumber = inputBonusNumber();
+                outputView.printInputMainNumbersMessage();
+                LottoNumbers mainNumbers = inputLottoNumbers();
+                outputView.printInputBonusBall();
+                LottoNumber bonusNumber = inputLottoNumber();
                 return new LottoResult(mainNumbers, bonusNumber);
             } catch (IllegalArgumentException e) {
                 outputView.printErrorMessage(e);
@@ -50,11 +58,61 @@ public class MainController {
         }
     }
 
-    private LottoNumbers inputMainNumbers() {
+    private int inputPrice() {
         while (true) {
             try {
-                outputView.printInputMainNumbersMessage();
-                List<Integer> numbers = inputView.getMainNumbers();
+                int price = inputView.getNumber();
+                if (price < 0) {
+                    throw new IllegalArgumentException("음수로는 구매할 수 없습니다.");
+                }
+                if (price == 0) {
+                    throw new IllegalArgumentException("0원으로는 구매할 수 없습니다.");
+                }
+                if (price % COST != 0) {
+                    throw new IllegalArgumentException(COST + "원 단위로 입력해주세요.");
+                }
+                return price;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e);
+            }
+        }
+    }
+
+    private int inputManualCount(int price) {
+        while (true) {
+            try {
+                int manualCount = inputView.getNumber();
+                if (manualCount < 0) {
+                    throw new IllegalArgumentException("음수로는 구매할 수 없습니다.");
+                }
+                if (manualCount * COST > price) {
+                    throw new IllegalArgumentException("지불한 금액보다 많이 구매할 수 없습니다.");
+                }
+                return manualCount;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e);
+            }
+        }
+    }
+
+    private List<LottoNumbers> inputLottoNumbersList(int manualCount) {
+        while (true) {
+            try {
+                List<LottoNumbers> lottoNumbersList = new ArrayList<>();
+                for (int i = 0; i < manualCount; i++) {
+                    lottoNumbersList.add(inputLottoNumbers());
+                }
+                return lottoNumbersList;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e);
+            }
+        }
+    }
+
+    private LottoNumbers inputLottoNumbers() {
+        while (true) {
+            try {
+                List<Integer> numbers = inputView.getLottoNumbers();
                 List<LottoNumber> lottoNumbers = new ArrayList<>();
                 for (Integer number : numbers) {
                     lottoNumbers.add(new LottoNumber(number));
@@ -66,10 +124,9 @@ public class MainController {
         }
     }
 
-    private LottoNumber inputBonusNumber() {
+    private LottoNumber inputLottoNumber() {
         while (true) {
             try {
-                outputView.printInputBonusBall();
                 int number = inputView.getNumber();
                 return new LottoNumber(number);
             } catch (IllegalArgumentException e) {
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -11,8 +11,16 @@ public class OutputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
+    public void printInputManualCountMessage() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+    }
+
+    public void printInputManualNumbersMessage() {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+    }
+
     public void printLottos(Lottos lottos) {
-        System.out.println(lottos.getLottos().size() + "개를 구매했습니다.");
+        System.out.printf("수동으로 %d장, 자동으로 %d개를 구매했습니다.\n", lottos.getManualCount(), lottos.getAutoCount());
         for (Lotto lotto : lottos.getLottos()) {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append("[");
