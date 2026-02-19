b02b50b97600c29fd87ee17a9ecca379df41cdb0
youngho <161284817+youngho98@users.noreply.github.com>
2026-02-19 10:54:26 +0900

    feat: Lottos 생성 시 수동으로 번호를 입력받을 수 있게 구현

--- src/main/java/model/Lottos.java
+++ src/main/java/model/Lottos.java
@@ -9,20 +9,37 @@ public class Lottos {
     public static final int COST = 1000;
 
     private final List<Lotto> lottos;
+    private final int manualCount;
+    private final int autoCount;
 
     public Lottos(int price) {
+        this(price, 0, null);
+    }
+
+    public Lottos(int price, int manualCount, List<LottoNumbers> lottoNumbersList) {
         validate(price);
-        int ticketCount = price / COST;
+        this.manualCount = manualCount;
+        this.autoCount = price / COST - manualCount;
         List<Lotto> lottos = new ArrayList<>();
-        while (lottos.size() < ticketCount) {
-            Lotto lotto = issue();
-            lottos.add(lotto);
+        for (int i = 0; i < manualCount; i++) {
+            lottos.add(new Lotto(lottoNumbersList.get(i)));
+        }
+        for (int i = 0; i < autoCount; i++) {
+            lottos.add(issue());
         }
         this.lottos = lottos;
     }
 
     public List<Lotto> getLottos() {
         return lottos;
+    }
+
+    public int getManualCount() {
+        return manualCount;
+    }
+
+    public int getAutoCount() {
+        return autoCount;
     }
 
     private Lotto issue() {
