1fac2f472c2f8bc6bfe9b5f33a0b8c498d0cd109
charlie.woo <dncjf1685@gmail.com>
2026-02-19 15:42:50 +0900

    refactor: 자동 로또 생성 로직 0장까지 생성 가능하도록 변경

--- src/main/java/lotto/model/ticket/TicketRandomGeneratorCommand.java
+++ src/main/java/lotto/model/ticket/TicketRandomGeneratorCommand.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public record TicketRandomGeneratorCommand(int count, Random random) implements TicketGeneratorCommand{
     public TicketRandomGeneratorCommand{
-        if(count <= 0) throw new IllegalArgumentException("발행하려는 티켓 수는 1 이상이어야합니다.");
+        if(count < 0) throw new IllegalArgumentException("발행하려는 티켓 수는 0 이상이어야합니다.");
         Objects.requireNonNull(random);
     }
     public TicketRandomGeneratorCommand(int count) {
